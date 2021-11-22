faed5157c46e29d44f705463f19373e767982833
NamarII <76418571+NamarII@users.noreply.github.com>
2021-11-21 23:44:50 -0300

    Add files via upload

---
+++ 4cac.py
@@ -0,0 +1,9 @@
+#Escribir una funcion que reciba un string con nombre y apellido
+#y devuelva un string con el nombre y apellido
+#pero con capitalizacion(primera letra mayuscula).
+nombre = input('Ingrese su nombre: ')
+apellido = input('Ingrese su apellido: ')
+string = nombre.capitalize() + ' ' + apellido.capitalize()
+print(string)
+
+
