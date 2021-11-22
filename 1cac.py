eaa1105a4d820278806c1d3c1e11dfad6c8741ea
NamarII <76418571+NamarII@users.noreply.github.com>
2021-11-21 23:27:44 -0300

    Add files via upload

---
+++ 1cac.py
@@ -0,0 +1,4 @@
+#Dado un string, escribir una funcion que cambie todos los espacios por guiones.
+string='Hola Mundo hola mundo HOLA MUNDO Hola Mundo hola mundo HOLA MUNDO Hola Mundo hola mundo HOLA MUNDO Hola Mundo hola mundo HOLA MUNDO Hola Mundo hola mundo HOLA MUNDO Hola Mundo hola mundo HOLA MUNDO Hola Mundo hola mundo HOLA MUNDO'
+mi_string = string.replace(' ', '-')
+print(mi_string)
