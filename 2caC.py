19703972ac59f40b6bb4d5c0a7528119e8d94a2d
NamarII <76418571+NamarII@users.noreply.github.com>
2021-11-21 23:53:42 -0300

    Update 2caC.py

--- 2caC.py
+++ 2caC.py
@@ -2,9 +2,6 @@
 #Tiene como limite 100 caracteres.
 string='Hola Mundo hola mundo HOLA MUNDO Hola Mundo hola mundo HOLA MUNDO Hola Mundo hola mundo HOLA MUNDO Hola Mundo hola mundo HOLA MUNDO Hola Mundo hola mundo HOLA MUNDO Hola Mundo hola mundo HOLA MUNDO Hola Mundo hola mundo HOLA MUNDO'
 string_swaped=string.swapcase()
-i=0
-for i in range(101):
-    i+=1
-print(string_swaped)
+print(string_swaped[:101])
 
 
